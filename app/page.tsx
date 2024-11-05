0f3b733a51c8fb1af487c42c03babfdd632c4f34
ankitku3101 <ankit.ku3101@gmail.com>
2024-11-06 04:06:53 +0530

    fixed margin issues on small screens #7

--- app/page.tsx
+++ app/page.tsx
@@ -7,21 +7,21 @@ import StickyHeader from "@/components/StickyHeader";
 export default function Home() {
   return (
     <div className="min-h-screen bg-custom-gradient">
-      <div className="text-center py-14">
-        <h1 className="text-4xl md:text-5xl font-bold tracking-tighter">
+      <div className="container text-center py-14">
+        <h1 className="text-3xl md:text-5xl font-bold tracking-tighter">
           Just-Another-<span className="text-[#1E6AE1]">UI</span>-Library
         </h1>
-        <p className="text-md md:text-lg tracking-tighter pb-6">A collection of all my designed cards and components.</p>
+        <p className="text-sm md:text-lg tracking-tighter pb-6">A collection of all my designed cards and components.</p>
         <GitHubButton />
       </div>
       <div className="flex flex-col items-center">
-        <div className="flex items-center justify-center p-10">
+        <div className="flex items-center justify-center py-10">
           <StickyHeader />
         </div>
-        <div className="flex items-center justify-center p-10">
+        <div className="flex items-center justify-center py-10">
           <GlassmorphicCard />
         </div>
-        <div className="flex items-center justify-center p-10">
+        <div className="flex items-center justify-center py-10">
           <GradientText />
         </div>
       </div>
--- components/GradientText.tsx
+++ components/GradientText.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function GradientText() {
   return (
     <div>
-        <h1 className="text-5xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
+        <h1 className="text-3xl md:text-5xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
               Gradient Text
         </h1>
     </div>
